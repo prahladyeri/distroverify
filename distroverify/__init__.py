178d2b53db070f8b3d8c3b3800f03e6ba9f9e18a
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-14 09:40:18 +0530

    fixed console script generation in setup.py

--- distroverify/__init__.py
+++ distroverify/__init__.py
@@ -3,6 +3,6 @@ __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
 __license__ = "MIT"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __title__ = 'Distro Verify'
 __description__ = 'Utility to verify any linux distro file (*.iso) for its integrity'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ s = setup(
 	packages=find_packages(),
 	include_package_data=True,
 	entry_points={
-		"distroverify": [
+		"console_scripts": [
 			"distroverify = distroverify.main:main",
 		],
 	},
