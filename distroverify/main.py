ff04e96cf65a8d1c99ae0f3b44a887d69857a99d
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-26 12:37:08 +0530

    feat: added support for opensuse-leap

--- distroverify/main.py
+++ distroverify/main.py
@@ -7,12 +7,20 @@ import json
 import re
 from distroverify import __title__, __description__, __version__
 
+# examples:
+# openSUSE-Leap-15.1-KDE-Live-x86_64-Current.iso
+# openSUSE-Leap-15.1-GNOME-Live-x86_64-Current.iso
+# debian-live-9.9.0-amd64-gnome.iso
+# ubuntu-16.04.6-desktop-i386.iso
+# ubuntu-16.04.6-server-i386.iso
+
 patterns = {
 	'ubuntu-mate': r'ubuntu-mate-(.*)-(.*)-(.*)\.iso',
 	'ubuntu-gnome': r'ubuntu-gnome-(.*)-(.*)-(.*)\.iso',
 	'xubuntu': r'xubuntu-(.*)-(.*)-(.*)\.iso',
 	'lubuntu': r'lubuntu-(.*)-(.*)-(.*)\.iso',
 	'ubuntu': r'ubuntu-(.*)-(.*)-(.*)\.iso',
+	'opensuse-leap': r'openSUSE-Leap-(.*)-(.*)-(.*)-(.*)-Current\.iso',
 }
 urls = {
 	'ubuntu-mate': 'http://cdimage.ubuntu.com/ubuntu-mate/releases/%s/release/SHA1SUMS',
@@ -20,6 +28,7 @@ urls = {
 	'xubuntu': 'http://cdimage.ubuntu.com/xubuntu/releases/%s/release/SHA1SUMS',
 	'lubuntu': 'http://cdimage.ubuntu.com/lubuntu/releases/%s/release/SHA1SUMS',
 	'ubuntu': 'http://cdimage.ubuntu.com/ubuntu/releases/%s/release/SHA1SUMS',
+	'opensuse-leap': 'https://download.opensuse.org/distribution/leap/%s/%s/'
 }
 
 def fileexists(filepath):
@@ -32,9 +41,24 @@ def fileexists(filepath):
 	except Exception as ex:
 			print(ex)
 
-def verify(distro, release_version, file_name, full_file_name):
+def verify(match, distro, file_name, full_file_name):
+	# print("release_version: %s" % release_version)
+	# print("ds: %s" % ds)
+	# print("arch: %s" % arch)
+
 	if distro in ['ubuntu', 'ubuntu-mate', 'xubuntu', 'kubuntu', 'lubuntu', 'ubuntu-budgie', 'ubuntu-gnome', 'ubuntu-core', 'ubuntu-server', 'ubuntu-touch', 'ubuntu-touch-custom', 'ubuntu-kylin', 'ubuntu-studio']:
-		url = urls[distro] % release_version
+		ver = match.groups()[0]
+		typ = match.groups()[1]
+		arch = match.groups()[2]
+		url = urls[distro] % ver
+		print("version: %s, type: %s, arch: %s" % (ver, typ, arch))
+	elif distro == 'opensuse-leap':
+		ver = match.groups()[0]
+		typ = match.groups()[1]
+		dist = match.groups()[2]
+		arch = match.groups()[3]
+		url = (urls[distro] % (ver, dist.lower())) + file_name + ".sha1"
+		print("version: %s, type: %s, dist: %s, arch: %s" % (ver, typ, dist, arch))
 	else:
 		print("unknown distro")
 		return
@@ -46,30 +70,38 @@ def verify(distro, release_version, file_name, full_file_name):
 			hash.update(chunk)
 	strhash = hash.hexdigest()
 	strhash = strhash.strip()
-	print('done')
+	print('done. fetching official hash')
 	resp = requests.get(url)
 	ss = resp.text
 	#print(ss.split("\n"))
-	for item in ss.split("\n"):
-		#print('comparing: ', item, file_name)
-		if item.endswith(file_name):
-			urlhash = item.split(" ")[0]
-			print('response hash:',urlhash)
-			print('calculated hash:', strhash)
-			print('match: ', urlhash == strhash)
-			return
-	print("hash not found in the response file")
+	is_found = False
+	if distro in ['opensuse-leap']:
+		urlhash = ss.split(" ")[0]
+		is_found = True
+		# print('ss.split():', ss.split())
+		# for line in ss.split():
+			# if file_name in line:
+				# is_found = True
+				# urlhash = line.split()[0]
+				# break
+	else: # ubuntu family of distros
+		for item in ss.split("\n"):
+			if item.endswith(file_name):
+				urlhash = item.split(" ")[0]
+				is_found = True
+				break
+	if is_found:
+		is_correct = (urlhash == strhash)
+		print('calculated hash:', strhash)
+		print('response hash:',urlhash)
+		print('match: ', is_correct)
+	else:
+		print("hash not found in the response file")
 
 def main():
-	banner = """%s version %s
-%s
-
-Copyright (c) 2019 Prahlad Yeri.
-
-This work is licensed under the terms of the MIT license.  
-For a copy, see <https://opensource.org/licenses/MIT>.
-""" % (__title__, __version__, __description__)
-	print(banner)
+	if '-v' in sys.argv or '--version' in sys.argv:
+		print("%s version %s" % (__title__, __version__))
+		return
 	parser = argparse.ArgumentParser()
 	parser.add_argument('distro_file', type=fileexists, help='Distro File Location EX: /Desktop/Somewhere/ubuntu-mate-19.04-desktop-amd64.iso')
 	parser.add_argument('-v', '--version', help='Version', action='store_true')
@@ -92,13 +124,7 @@ For a copy, see <https://opensource.org/licenses/MIT>.
 		sys.exit()
 	else:
 		print("match success: ", distro)
-		release_version = match.groups()[0]
-		ds = match.groups()[1]
-		arch = match.groups()[2]
-		# print("release_version: %s" % release_version)
-		# print("ds: %s" % ds)
-		# print("arch: %s" % arch)
-		verify(distro, release_version, args.distro_file, full_file_name)
+		verify(match, distro, args.distro_file, full_file_name)
 		sys.exit()
 
 	#calculate hash of file
