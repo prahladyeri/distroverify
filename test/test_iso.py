291c1f7a4f1dad440e1d7b8b50110e6dcb42fadc
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-27 17:57:33 +0530

    chore: refactored filename

--- test/test_iso.py
+++ test/test_iso.py
@@ -1,10 +1,10 @@
-from distroverify import main
+from distroverify import distroverify
 import os
 
 def test_fake_iso():
 	for file in os.scandir(r"test"):
 		a = os.path.splitext(file.path)
 		if a[1] == '.iso': 
-			result = main.process([file.path])
+			result = distroverify.process([file.path])
 			assert result == False or result == None
 			
