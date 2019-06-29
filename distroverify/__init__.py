510950f189b5e20558b3e473e93940490a8fc812
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-29 10:49:03 +0530

    chore: released 1.0.6

--- distroverify/__init__.py
+++ distroverify/__init__.py
@@ -3,6 +3,6 @@ __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
 __license__ = "MIT"
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __title__ = 'Distro Verify'
 __description__ = 'Utility to verify any linux distro file (*.iso) for its integrity'
