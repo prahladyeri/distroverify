7fe7f9c4e099c269544381b21a6c7c0a34aa5036
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-30 18:38:33 +0530

    build: released 1.0.8

--- distroverify/__init__.py
+++ distroverify/__init__.py
@@ -3,6 +3,6 @@ __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
 __license__ = "MIT"
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 __title__ = 'Distro Verify'
 __description__ = 'Utility to verify any linux distro file (*.iso) for its integrity'
