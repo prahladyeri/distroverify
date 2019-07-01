045c75afd83f53745431e1b4b83ab7dd7dd3a758
Prahlad Yeri <prahladyeri@yahoo.com>
2019-07-01 12:50:12 +0530

    test: moved fake iso files to a separate directory

--- test/test_iso.py
+++ test/test_iso.py
@@ -2,7 +2,7 @@ from distroverify import distroverify
 import os
 
 def test_fake_iso():
-	for file in os.scandir(r"test"):
+	for file in os.scandir("test/fake_iso"):
 		a = os.path.splitext(file.path)
 		if a[1] == '.iso': 
 			result = distroverify.process([file.path])
