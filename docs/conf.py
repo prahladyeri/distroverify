5bae2bd0012a472606b612f2dd82917c35011e5d
Prahlad Yeri <prahladyeri@yahoo.com>
2023-11-15 11:42:25 +0530

    chore: updated docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -53,11 +53,14 @@ extensions = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_theme = 'pyramid'
+#html_theme = 'agogo'
+#html_theme = 'bizstyle'
+#html_theme = 'master'
 #html_theme = 'alabaster'
 #html_theme = 'sphinxdoc'
 #html_theme = 'classic'
 #html_theme = 'nature'
-#html_theme = 'pyramid'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
